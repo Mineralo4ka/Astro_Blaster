9f3211184c1a8f9038f36225d1fb85a9c68668c5
Mineralo4ka <andrew.pavlenko@mail.ru>
2017-04-16 22:09:34 +0700

    Add function ship, ver 0.2

--- js/script.js
+++ js/script.js
@@ -1,15 +1,56 @@
 var canvas = document.getElementById("example");
 var context = canvas.getContext("2d");
 
-function ship()
-{
-	context.beginPath();
-	context.moveto(80, 40);
-	context.lineto(120, 40);
-	context.lineto(100, 50);
-	context.closePath();
-	context.fillStyle = "#20B19C";
-    context.fill();
-    context.strokeStyle = "#20B19C";
-    context.stroke();
+//var ship = new Array(190, 710, 190, 780, 200, 770, 200, 755, 210, 750, 210, 780, 220, 780, 220, 785, 230, 785, 230, 780, 240, 780, 240, 750, 250, 755, 250, 770, 260, 780, 260, 710, 
+//250, 710, 250, 740, 240, 730, 230, 710, 230, 690, 220, 690, 220, 710, 210, 730, 200, 740, 200, 710, 190, 710);
+var ship_x = new Array(190, 190, 200, 200, 210, 210, 220, 220, 230, 230, 240, 240, 250, 250, 260, 260, 250, 250, 240, 230, 230, 220, 220, 210, 200, 200, 190);
+
+var ship_y = new Array(710, 780, 770, 755, 750, 780, 780, 785, 785, 780, 780, 750, 755, 770, 780, 710, 710, 740, 730, 710, 690, 690, 710, 730, 740, 710, 710);
+
+var monster = new Array(190, 100, 205, 80, 220, 80, 235, 100, 230, 105, 195, 105, 190, 100);
+
+function ship(dx, dy) {
+		this.dx = 0;
+    	this.dy = 0;
+    	this.health = 100;
+    	this.draw = function(x, y, c, cc) {
+    		var i = 0;
+			var l = ship_x.length;
+			context.fillStyle = c;
+			context.strokeStyle = cc;
+			context.beginPath();
+			context.moveTo(x[i], y[i]);
+			i = 2;
+			while (i < l) {
+				context.lineTo(x[i] , y[i]);
+				i = i + 1;
+			}
+			context.closePath();
+			context.fill();
+			context.stroke();
+    	}
+}
+
+k = new ship(0, 0);
+k.draw(ship_x, ship_y, "#21375F", "#FFB200");
+
+onkeypress = function() {
+		if(event.keyCode == 100) {//right
+			if(k.dx < 600) {
+				k.dx += 35;
+			}
+		}
+		if(event.keyCode == 97) {//left
+			if(k.dx > 600) {
+				k.dx-=35;
+			}
+		}
+		k.draw(ship_x, ship_y, "#21375F", "#FFB200");
+}
+
+setInterval(function() {
+}, 50);
+
+function Clear(dx, dy) {
+  context.clearRect(0,0,window.innerWidth,window.innerHeight);
 }
